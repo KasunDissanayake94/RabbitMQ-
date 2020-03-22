e01d4d4155e3b232cc45c6b7ce12b79d4480af55
kasund <kasund@hsenidlanka.com>
2020-03-22 21:27:21 +0530

    topic exchange added

--- src/main/java/com/example/rabbitmq/config/RabbitMQFanoutConfig.java
+++ src/main/java/com/example/rabbitmq/config/RabbitMQFanoutConfig.java
@@ -1,50 +1,50 @@
-package com.example.rabbitmq.config;
-
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQFanoutConfig {
-
-    @Bean
-    Queue marketingQueue() {
-        return new Queue("marketingQueue", false);
-    }
-
-    @Bean
-    Queue financeQueue() {
-        return new Queue("financeQueue", false);
-    }
-    @Bean
-    Queue adminQueue() {
-        return new Queue("adminQueue", false);
-    }
-    @Bean
-    FanoutExchange exchange() {
-        return new FanoutExchange("fanout-exchange");
-    }
-
-    @Bean
-    Binding marketingBinding(Queue marketingQueue, FanoutExchange exchange) {
-        return BindingBuilder.bind(marketingQueue).to(exchange);
-
-    }
-
-    @Bean
-    Binding financeBinding(Queue financeQueue, FanoutExchange exchange) {
-        return BindingBuilder.bind(financeQueue).to(exchange);
-
-    }
-
-    @Bean
-    Binding adminBinding(Queue adminQueue, FanoutExchange exchange) {
-        return BindingBuilder.bind(adminQueue).to(exchange);
-
-    }
-}
+//package com.example.rabbitmq.config;
+//
+//import org.springframework.amqp.support.converter.MessageConverter;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.core.*;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQFanoutConfig {
+//
+//    @Bean
+//    Queue marketingQueue() {
+//        return new Queue("marketingQueue", false);
+//    }
+//
+//    @Bean
+//    Queue financeQueue() {
+//        return new Queue("financeQueue", false);
+//    }
+//    @Bean
+//    Queue adminQueue() {
+//        return new Queue("adminQueue", false);
+//    }
+//    @Bean
+//    FanoutExchange exchange() {
+//        return new FanoutExchange("fanout-exchange");
+//    }
+//
+//    @Bean
+//    Binding marketingBinding(Queue marketingQueue, FanoutExchange exchange) {
+//        return BindingBuilder.bind(marketingQueue).to(exchange);
+//
+//    }
+//
+//    @Bean
+//    Binding financeBinding(Queue financeQueue, FanoutExchange exchange) {
+//        return BindingBuilder.bind(financeQueue).to(exchange);
+//
+//    }
+//
+//    @Bean
+//    Binding adminBinding(Queue adminQueue, FanoutExchange exchange) {
+//        return BindingBuilder.bind(adminQueue).to(exchange);
+//
+//    }
+//}
--- src/main/java/com/example/rabbitmq/controller/RabbitMQFanoutWebController.java
+++ src/main/java/com/example/rabbitmq/controller/RabbitMQFanoutWebController.java
@@ -1,26 +1,26 @@
-package com.example.rabbitmq.controller;
-
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/example-rabbitmq/fanout/")
-public class RabbitMQFanoutWebController {
-
-    @Autowired
-    private AmqpTemplate amqpTemplate;
-    //this will send message to Rmq exchange
-
-    @GetMapping(value = "/producer")
-    public String producer(@RequestParam("exchangeName") String exchange, @RequestParam("messageData") String messageData) {
-
-        amqpTemplate.convertAndSend(exchange, "", messageData);
-
-        return "Message sent to the RabbitMQ FanOut Successfully";
-    }
-
-}
+//package com.example.rabbitmq.controller;
+//
+//import org.springframework.amqp.core.AmqpTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(value = "/example-rabbitmq/fanout/")
+//public class RabbitMQFanoutWebController {
+//
+//    @Autowired
+//    private AmqpTemplate amqpTemplate;
+//    //this will send message to Rmq exchange
+//
+//    @GetMapping(value = "/producer")
+//    public String producer(@RequestParam("exchangeName") String exchange, @RequestParam("messageData") String messageData) {
+//
+//        amqpTemplate.convertAndSend(exchange, "", messageData);
+//
+//        return "Message sent to the RabbitMQ FanOut Successfully";
+//    }
+//
+//}
