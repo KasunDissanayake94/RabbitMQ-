c448675ef42a72a5683ea08ea3ff13b80916c4f9
kasund <kasund@hsenidlanka.com>
2020-03-22 21:55:34 +0530

    Header exchange completed

---
+++ src/main/java/com/example/rabbitmq/config/RabbitMQHeaderConfig.java
@@ -0,0 +1,50 @@
+package com.example.rabbitmq.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQHeaderConfig {
+
+    @Bean
+    Queue marketingQueue() {
+        return new Queue("marketingQueue", false);
+    }
+
+    @Bean
+    Queue financeQueue() {
+        return new Queue("financeQueue", false);
+    }
+    @Bean
+    Queue adminQueue() {
+        return new Queue("adminQueue", false);
+    }
+
+
+
+    @Bean
+    HeadersExchange exchange() {
+        return new HeadersExchange("header-exchange");
+    }
+
+    @Bean
+    Binding marketingBinding(Queue marketingQueue, HeadersExchange exchange) {
+        return BindingBuilder.bind(marketingQueue).to(exchange).where("department").matches("marketing");
+
+    }
+
+    @Bean
+    Binding financeBinding(Queue financeQueue, HeadersExchange exchange) {
+        return BindingBuilder.bind(financeQueue).to(exchange).where("department").matches("finance");
+
+    }
+
+    @Bean
+    Binding adminBinding(Queue adminQueue, HeadersExchange exchange) {
+        return BindingBuilder.bind(adminQueue).to(exchange).where("department").matches("admin");
+
+    }
+
+}
+
--- src/main/java/com/example/rabbitmq/config/RabbitMQTopicConfig.java
+++ src/main/java/com/example/rabbitmq/config/RabbitMQTopicConfig.java
@@ -1,62 +1,62 @@
-package com.example.rabbitmq.config;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQTopicConfig {
-
-    @Bean
-    Queue marketingQueue() {
-        return new Queue("marketingQueue", false);
-    }
-
-    @Bean
-    Queue financeQueue() {
-        return new Queue("financeQueue", false);
-    }
-    @Bean
-    Queue adminQueue() {
-        return new Queue("adminQueue", false);
-    }
-
-    @Bean
-    Queue allQueue() {
-        return new Queue("allQueue", false);
-    }
-
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange("topic-exchange");
-    }
-
-    @Bean
-    Binding marketingBinding(Queue marketingQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(marketingQueue).to(exchange).with("queue.marketing");
-
-    }
-
-    @Bean
-    Binding financeBinding(Queue financeQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(financeQueue).to(exchange).with("queue.finance");
-
-    }
-
-    @Bean
-    Binding adminBinding(Queue adminQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(adminQueue).to(exchange).with("queue.admin");
-
-    }
-
-    @Bean
-    Binding allBinding(Queue allQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(allQueue).to(exchange).with("queue.*");
-
-    }
-}
+//package com.example.rabbitmq.config;
+//import org.springframework.amqp.support.converter.MessageConverter;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.core.*;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQTopicConfig {
+//
+//    @Bean
+//    Queue marketingQueue() {
+//        return new Queue("marketingQueue", false);
+//    }
+//
+//    @Bean
+//    Queue financeQueue() {
+//        return new Queue("financeQueue", false);
+//    }
+//    @Bean
+//    Queue adminQueue() {
+//        return new Queue("adminQueue", false);
+//    }
+//
+//    @Bean
+//    Queue allQueue() {
+//        return new Queue("allQueue", false);
+//    }
+//
+//
+//    @Bean
+//    TopicExchange exchange() {
+//        return new TopicExchange("topic-exchange");
+//    }
+//
+//    @Bean
+//    Binding marketingBinding(Queue marketingQueue, TopicExchange exchange) {
+//        return BindingBuilder.bind(marketingQueue).to(exchange).with("marketing");
+//
+//    }
+//
+//    @Bean
+//    Binding financeBinding(Queue financeQueue, TopicExchange exchange) {
+//        return BindingBuilder.bind(financeQueue).to(exchange).with("queue.finance");
+//
+//    }
+//
+//    @Bean
+//    Binding adminBinding(Queue adminQueue, TopicExchange exchange) {
+//        return BindingBuilder.bind(adminQueue).to(exchange).with("queue.admin");
+//
+//    }
+//
+//    @Bean
+//    Binding allBinding(Queue allQueue, TopicExchange exchange) {
+//        return BindingBuilder.bind(allQueue).to(exchange).with("queue.*");
+//
+//    }
+//}
